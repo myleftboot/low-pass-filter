27acb5ec42e78ebfd434825d21ac73c669e8d6f0
Darren Cope <github@darren.cope.name>
2013-01-10 07:47:29 +0000

    tidied up

--- Resources/app.js
+++ Resources/app.js
@@ -4,7 +4,29 @@ Titanium.UI.setBackgroundColor('#000');
 // create tab group
 var tabGroup = Titanium.UI.createTabGroup();
 
-Ti.include('low_pass_adaptive.js');
+var noiseAttenuation = 3;
+var accelerometerMinStep = 0.02;
+var filterConstant = 0.2;
+
+var alpha     = 0;
+var lastValue = 0;
+
+clamp = function(val) {
+	if (val > 1) return 1
+	if (val < 0) return 0;
+	return val;
+}
+
+lowPassFilter = function(val)
+{
+    var diff = clamp(Math.abs(lastValue - val) / accelerometerMinStep);
+    
+    alpha = (1.0 - diff) * filterConstant / noiseAttenuation + diff * filterConstant
+    
+    lastValue = val * alpha + lastValue * (1.0 - alpha);
+    
+    return lastValue;
+}
 //
 // create base UI tab and root window
 //
